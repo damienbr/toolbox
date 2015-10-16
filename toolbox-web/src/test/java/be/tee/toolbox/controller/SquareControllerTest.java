f97250a821d90ada99ed789e673d544d8e8e4861
damienbr <damien.bravin@takeeateasy.be>
2015-10-16 17:52:25 +0200

    toolbox uitesting

--- toolbox-web/src/test/java/be/tee/toolbox/controller/SquareControllerTest.java
+++ toolbox-web/src/test/java/be/tee/toolbox/controller/SquareControllerTest.java
@@ -5,14 +5,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @WebAppConfiguration
-@ContextConfiguration("application-test-context.xml")
+@ContextConfiguration("classpath:/application-test-context.xml")
 public class SquareControllerTest extends AbstractTestNGSpringContextTests {
+
+    private static String EXPECTED_MESSAGE = "<br><div style='text-align:center;'><h3>Hello World, Spring MVC Tutorial</h3>Hello takeeateasy!</div><br><br>";
 
     @Autowired
     private WebApplicationContext wac;
@@ -26,7 +30,10 @@ public class SquareControllerTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testHelloWorld() throws Exception {
-
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/square"))
+                .andExpect(MockMvcResultMatchers.view().name("welcome"))
+                .andExpect(MockMvcResultMatchers.model().attribute("message", EXPECTED_MESSAGE))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
